cd05f7851185072d37a63431b863f83e2b2f54d2
ArsalonAmini <aminihajibasa@gmail.com>
2016-08-10 18:17:05 -0500

    baseball statistics array created  //Function #2 function to calculate a player's slugging percentage

---
+++ BaseballArray/BaseballArray/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballArray
+{
+    class Program
+    {
+        public int playerSlug { get; private set; }
+        int single;
+        int doubles;
+        int triple;
+        int home;
+        int batNumber;
+
+        static void Main(string[] args)
+        {
+            //user input stats
+            Console.WriteLine("Please input single's statistics");
+            single = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input doubles statistics");
+            doubles = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input triple statistics");
+            triple = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input home run statistics");
+            home = Convert.ToInt32(Console.ReadLine()); }
+
+            //Function #1 store players stats inside an array 
+            public void StatArray(int single, int doubles, int triple, int home)
+        {
+  
+            int[] statArray = {single, doubles, triple, home};
+            Console.WriteLine(statArray);
+        }
+        //Function #2 function to calculate a player's slugging percentage
+        public void slugPercent(double[] statArray)
+        {
+                playerSlug[] = (statArray[0] + 2 * doubles + 3 * triple + 4 * home) / (batNumber);
+                Console.WriteLine(playerSlug[i]);
+            }
+        }
+
+        }
+    }
+
+
---
+++ BaseballArray/BaseballArray/Slug.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballArray
+{
+    class Slug
+    {
+        public int playerSlug { get; private set; }
+        int single;
+        int doubles;
+        int triple;
+        int home;
+        int batNumber;
+
+        //user input stats
+        Console.WriteLine("Please input single's statistics");
+            single = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input doubles statistics");
+            doubles = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input triple statistics");
+            triple = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input home run statistics");
+            home = Convert.ToInt32(Console.ReadLine()); }
+            Console.WriteLine("Please input bat number");
+            bat = Convert.ToInt32(Console.ReadLine());
+
+    public void StatArray(int single, int doubles, int triple, int home, int bat)
+    {
+
+        int[] statArray = { single, doubles, triple, home, bat};
+        Console.WriteLine(statArray);
+    }
+
+    public void slugPercent(double[] statArray)  //Function #2 function to calculate a player's slugging percentage
+    {
+        playerSlug[] = (statArray[0] + 2 * statArray[1] + 3 * statArray[2] + 4 * statArray[3]) / (statArray[4]);
+        Console.WriteLine(playerSlug[i]);
+    }
+ }
+
+ 
